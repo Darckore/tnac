04f10b0ddd1d9bc9af80b4814f376fea23565b56
darckore <darckore@gmail.com>
2023-03-29 12:39:46 +0300

    base declarator node

--- tnac_lib/include/ast/ast_decls.hpp
+++ tnac_lib/include/ast/ast_decls.hpp
@@ -4,8 +4,34 @@
 
 #pragma once
 #include "ast/ast.hpp"
+#include "ast/ast_expr.hpp"
 
 namespace tnac::ast
 {
+  //
+  // Base declarator
+  //
+  class decl : public node
+  {
+  private:
+    friend class builder;
 
+  public:
+    CLASS_SPECIALS_NONE(decl);
+
+    virtual ~decl() noexcept;
+
+  protected:
+    decl(kind k, id_expr* id, expr* def) noexcept;
+
+  public:
+    string_t name() const noexcept;
+
+    const expr* definition() const noexcept;
+    expr* definition() noexcept;
+
+  private:
+    id_expr* m_id{};
+    expr* m_def{};
+  };
 }
--- tnac_lib/src/ast/ast_decls.cpp
+++ tnac_lib/src/ast/ast_decls.cpp
@@ -2,5 +2,30 @@
 
 namespace tnac::ast
 {
+  // Base declarator
 
+  decl::~decl() noexcept = default;
+
+  decl::decl(kind k, id_expr* id, expr* def) noexcept :
+    node{ k },
+    m_id{ id },
+    m_def{ def }
+  {
+    assume_ancestry(m_id);
+    assume_ancestry(m_def);
+  }
+
+  string_t decl::name() const noexcept
+  {
+    return m_id->name();
+  }
+
+  const expr* decl::definition() const noexcept
+  {
+    return m_def;
+  }
+  expr* decl::definition() noexcept
+  {
+    return utils::mutate(std::as_const(*this).definition());
+  }
 }
