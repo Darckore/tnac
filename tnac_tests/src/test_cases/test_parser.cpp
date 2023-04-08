7875b1696d0dda64829cbb6ee80b6ab522a00b2f
darckore <darckore@gmail.com>
2023-04-08 17:08:09 +0300

    parser tests

--- tnac_tests/src/test_cases/test_parser.cpp
+++ tnac_tests/src/test_cases/test_parser.cpp
@@ -1,5 +1,6 @@
 #include "parser/parser.hpp"
 #include "sema/sema.hpp"
+#include "ast/ast_visitor.hpp"
 
 namespace tnac_tests
 {
@@ -25,6 +26,108 @@ namespace tnac_tests
       tnac::parser parser;
     };
 
+    struct expected_node
+    {
+      string_t data{};
+      node_kind kind{};
+      node_kind parent{};
+      bool nullParent{};
+    };
+
+    class tree_checker : public tree::const_bottom_up_visitor<tree_checker>
+    {
+    public:
+      using test_data = std::span<expected_node>;
+      using data_iter = test_data::iterator;
+
+    public:
+      CLASS_SPECIALS_NODEFAULT(tree_checker);
+
+      tree_checker(test_data expected) noexcept :
+        m_data{ expected },
+        m_iter{ expected.begin() }
+      {}
+
+    public:
+      void visit(const tree::scope* scope) noexcept
+      {
+        check_node(scope, "");
+      }
+
+      void visit(const tree::assign_expr* expr) noexcept
+      {
+        check_node(expr, expr->op().m_value);
+      }
+
+      void visit(const tree::decl_expr* expr) noexcept
+      {
+        check_node(expr, "");
+      }
+
+      void visit(const tree::var_decl* decl) noexcept
+      {
+        check_node(decl, decl->name());
+      }
+
+      void visit(const tree::binary_expr* expr) noexcept
+      {
+        check_node(expr, expr->op().m_value);
+      }
+
+      void visit(const tree::unary_expr* expr) noexcept
+      {
+        check_node(expr, expr->op().m_value);
+      }
+
+      void visit(const tree::paren_expr* expr) noexcept
+      {
+        check_node(expr, "");
+      }
+
+      void visit(const tree::lit_expr* expr) noexcept
+      {
+        check_node(expr, expr->pos().m_value);
+      }
+
+      void visit(const tree::id_expr* expr) noexcept
+      {
+        check_node(expr, expr->name());
+      }
+
+      void visit(const tree::error_expr* expr) noexcept
+      {
+        check_node(expr, expr->message());
+      }
+
+    private:
+      void check_node(const tree::node* node, string_t nodeStr) noexcept
+      {
+        ASSERT_NE(node, nullptr) << "Null node detected";
+        ASSERT_NE(m_iter, m_data.end()) << "Unexpected end of data";
+        auto&& expected = *m_iter;
+        ++m_iter;
+
+        EXPECT_EQ(nodeStr, expected.data);
+
+        const auto expKind = node->what();
+        EXPECT_EQ(expKind, expected.kind) << "Wrong kind at node " << nodeStr;
+
+        auto parent = node->parent();
+        if (!parent)
+        {
+          EXPECT_TRUE(expected.nullParent) << "Parent was null at node " << nodeStr;
+        }
+        else
+        {
+          EXPECT_EQ(parent->what(), expected.parent) << "Wrong parent at node " << nodeStr;
+        }
+      }
+
+    private:
+      test_data m_data{};
+      data_iter m_iter{};
+    };
+
     template <node_kind kind, std::size_t N>
     void check_simple_exprs(const std::array<string_t, N>& inputs)
     {
@@ -46,6 +149,13 @@ namespace tnac_tests
           }
         }
       }
+    }
+
+    void check_tree_structute(std::span<expected_node> exp, string_t input) noexcept
+    {
+      parse_helper p;
+      auto ast = p(input);
+      tree_checker{ exp }(ast);
     }
   }
 
@@ -80,5 +190,235 @@ namespace tnac_tests
     using tnac::ast::node_kind;
     using detail::check_simple_exprs;
     check_simple_exprs<node_kind::Binary>(inputArr);
+  }
+
+  TEST(parser, t_struct_simple_unary)
+  {
+    using detail::expected_node;
+    using enum detail::node_kind;
+    constexpr auto input = "-42"sv;
+
+    /*
+          -'-'
+         |
+        42
+    */
+
+    std::array exp{
+      expected_node{ "42", Literal, Unary },
+      expected_node{  "-", Unary,   Scope }
+    };
+
+    detail::check_tree_structute(exp, input);
+  }
+
+  TEST(parser, t_struct_complex_unary)
+  {
+    using detail::expected_node;
+    using enum detail::node_kind;
+    constexpr auto input = "-(42 + 69)"sv;
+
+    /*
+                -'-'
+               |
+             -()
+            |
+          -'+'-
+         |     |
+        42     69
+    */
+
+    std::array exp{
+      expected_node{ "42", Literal, Binary },
+      expected_node{ "69", Literal, Binary },
+      expected_node{  "+", Binary,  Paren },
+      expected_node{   "", Paren,   Unary },
+      expected_node{  "-", Unary,   Scope },
+    };
+
+    detail::check_tree_structute(exp, input);
+  }
+
+  TEST(parser, t_struct_simple_binary)
+  {
+    using detail::expected_node;
+    using enum detail::node_kind;
+    constexpr auto input = "42 + -2"sv;
+
+    /*
+          -'+'-
+         |     |
+        42    '-'
+               |
+               2
+    */
+
+    std::array exp{
+      expected_node{ "42", Literal, Binary },
+      expected_node{  "2", Literal, Unary },
+      expected_node{  "-", Unary,   Binary },
+      expected_node{  "+", Binary,  Scope }
+    };
+
+    detail::check_tree_structute(exp, input);
+  }
+
+  TEST(parser, t_struct_seq_binary)
+  {
+    using detail::expected_node;
+    using enum detail::node_kind;
+    constexpr auto input = "128 - 127-1"sv;
+
+    /*
+             --'-'-
+            |      |
+          -'-'-    1
+         |     |
+        128   127
+    */
+
+    std::array exp{
+      expected_node{ "128", Literal, Binary },
+      expected_node{ "127", Literal, Binary },
+      expected_node{   "-", Binary,  Binary },
+      expected_node{   "1", Literal, Binary },
+      expected_node{   "-", Binary,  Scope },
+    };
+
+    detail::check_tree_structute(exp, input);
+  }
+
+  TEST(parser, t_struct_complex_binary)
+  {
+    using detail::expected_node;
+    using enum detail::node_kind;
+    constexpr auto input = "42 + -2 + 69 * 5 + 1"sv;
+
+    /*
+                       -------'+'--
+                      |            |
+              -------'+'--         1
+             |            |
+           -'+'-        -'*'- 
+          |     |      |     |
+         42    '-'    69     5
+                |
+                2
+    */
+
+    std::array exp{
+      expected_node{ "42", Literal, Binary },
+      expected_node{  "2", Literal, Unary },
+      expected_node{  "-", Unary,   Binary },
+      expected_node{  "+", Binary,  Binary },
+      expected_node{ "69", Literal, Binary },
+      expected_node{  "5", Literal, Binary },
+      expected_node{  "*", Binary,  Binary },
+      expected_node{  "+", Binary,  Binary },
+      expected_node{  "1", Literal, Binary },
+      expected_node{  "+", Binary,  Scope }
+    };
+
+    detail::check_tree_structute(exp, input);
+  }
+
+  TEST(parser, t_struct_op_precedence)
+  {
+    using detail::expected_node;
+    using enum detail::node_kind;
+    constexpr auto input = "42 * ( 1 + 2) + 2 * 69"sv;
+
+    /*
+              -----'+'---
+             |           |
+          --'*'--      -'*'-
+         |       |    |     |
+        42     -()    2     69
+              |
+            -'+'-
+           |     |
+           1     2
+    */
+
+    std::array exp{
+      expected_node{ "42", Literal, Binary },
+      expected_node{  "1", Literal, Binary },
+      expected_node{  "2", Literal, Binary },
+      expected_node{  "+", Binary,  Paren },
+      expected_node{   "", Paren,   Binary },
+      expected_node{  "*", Binary,  Binary },
+      expected_node{  "2", Literal, Binary },
+      expected_node{ "69", Literal, Binary },
+      expected_node{  "*", Binary,  Binary },
+      expected_node{  "+", Binary,  Scope },
+    };
+
+    detail::check_tree_structute(exp, input);
+  }
+
+  TEST(parser, t_struct_simple_decl)
+  {
+    using detail::expected_node;
+    using enum detail::node_kind;
+    constexpr auto input = "a = 42 + 1"sv;
+
+    /*
+                -decl-expr
+               |
+             -'a'
+            |
+          -'+'-
+         |     |
+        42     1
+    */
+
+    std::array exp{
+      expected_node{ "42", Literal, Binary },
+      expected_node{  "1", Literal, Binary },
+      expected_node{  "+", Binary,  VarDecl },
+      expected_node{  "a", VarDecl, Decl },
+      expected_node{   "", Decl,    Scope },
+    };
+
+    detail::check_tree_structute(exp, input);
+  }
+
+  TEST(parser, t_struct_complex_decl)
+  {
+    using detail::expected_node;
+    using enum detail::node_kind;
+    constexpr auto input = "a = b = c = 42 + 1"sv;
+
+    /*
+                                  -decl-expr
+                                 |
+                               -'a'
+                              |
+                         -decl-expr
+                        |
+                      -'b'
+                     |
+                -decl-expr
+               |
+             -'c'
+            |
+          -'+'-
+         |     |
+        42     1
+    */
+
+    std::array exp{
+      expected_node{ "42", Literal, Binary },
+      expected_node{  "1", Literal, Binary },
+      expected_node{  "+", Binary,  VarDecl },
+      expected_node{  "c", VarDecl, Decl },
+      expected_node{   "", Decl,    VarDecl },
+      expected_node{  "b", VarDecl, Decl },
+      expected_node{   "", Decl,    VarDecl },
+      expected_node{  "a", VarDecl, Decl },
+      expected_node{   "", Decl,    Scope },
+    };
+
+    detail::check_tree_structute(exp, input);
   }
 }
