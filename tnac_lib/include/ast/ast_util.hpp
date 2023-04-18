940738dc51aba17dee627f6fbb799a8449cfb3d9
darckore <darckore@gmail.com>
2023-04-19 00:06:03 +0300

    command class

--- tnac_lib/include/ast/ast_util.hpp
+++ tnac_lib/include/ast/ast_util.hpp
@@ -5,6 +5,11 @@
 #pragma once
 #include "ast/ast_expr.hpp"
 
+namespace tnac
+{
+  class parser;
+}
+
 namespace tnac::ast
 {
   //
@@ -39,4 +44,97 @@ namespace tnac::ast
     string_t m_errMsg;
   };
 
+
+  //
+  // Command
+  // Commands aren't actually included in the ast, or even returned as such
+  // They are instructions to the driver to perform certain tasks
+  // Declared here only because commands are produced in the parser, so they are
+  // logically sort of related to the ast
+  //
+  class command final
+  {
+  private:
+    friend class tnac::parser;
+
+    using param_list = std::vector<token>;
+    using iterator = param_list::const_iterator;
+    using size_type = param_list::size_type;
+
+    using param_baseline = std::span<const tok_kind>;
+
+  public:
+    //
+    // Indicates whether or not this command passes verification
+    //
+    enum class verification : std::uint8_t
+    {
+      Correct,
+      TooFew,
+      TooMany,
+      WrongKind
+    };
+
+    using enum verification;
+
+    //
+    // Verification result.
+    // 
+    // If verification equals WrongKind, m_diff contains the index of the
+    // first parameter which failed the check
+    // 
+    // Otherwise, if verification equals TooFew or TooMany, m_diff
+    // contains the actual number of parameters
+    //
+    struct verification_result
+    {
+      size_type m_diff{};
+      verification m_res{};
+    };
+
+  public:
+    CLASS_SPECIALS_NODEFAULT(command);
+
+    ~command() noexcept;
+
+  protected:
+    command(const token& cmd, param_list params) noexcept;
+
+  public:
+    //
+    // Returns the parameter by the given index
+    // Bounds checking is on the caller
+    //
+    const token& operator[](size_type idx) const noexcept;
+
+    //
+    // Returns the command name
+    //
+    string_t name() const noexcept;
+
+    //
+    // Returns the number of parameters
+    //
+    size_type param_count() const noexcept;
+
+    //
+    // Begin iterator to the param list
+    //
+    iterator begin() const noexcept;
+
+    //
+    // End iterator to the param list
+    //
+    iterator end() const noexcept;
+
+    //
+    // Verifies command structure (the number and kinds of params)
+    // Returns a verification_result instance
+    //
+    verification_result verify(param_baseline base) const noexcept;
+
+  private:
+    token m_cmd;
+    param_list m_params;
+  };
 }
--- tnac_lib/src/ast/ast_util.cpp
+++ tnac_lib/src/ast/ast_util.cpp
@@ -21,4 +21,70 @@ namespace tnac::ast
     return m_errMsg;
   }
 
+
+  // Command
+
+  command::~command() noexcept = default;
+
+  command::command(const token& cmd, param_list params) noexcept :
+    m_cmd{ cmd },
+    m_params{ std::move(params) }
+  {}
+
+  const token& command::operator[](size_type idx) const noexcept
+  {
+    return m_params[idx];
+  }
+
+  string_t command::name() const noexcept
+  {
+    return m_cmd.m_value;
+  }
+
+  command::size_type command::param_count() const noexcept
+  {
+    return m_params.size();
+  }
+
+  command::iterator command::begin() const noexcept
+  {
+    return m_params.begin();
+  }
+
+  command::iterator command::end() const noexcept
+  {
+    return m_params.end();
+  }
+
+  command::verification_result command::verify(param_baseline base) const noexcept
+  {
+    verification_result res;
+
+    const auto paramSize = param_count();
+    const auto baseSize  = base.size();
+
+    if (paramSize > baseSize)
+      res.m_res = TooMany;
+    else if (paramSize < baseSize)
+      res.m_res = TooFew;
+
+    if (res.m_res != Correct)
+      res.m_diff = paramSize;
+    else
+    {
+      auto idx = size_type{};
+      for ( ; idx < paramSize; ++idx)
+      {
+        if((*this)[idx].is(base[idx]))
+          continue;
+
+        res.m_res = WrongKind;
+        res.m_diff = idx;
+        break;
+      }
+    }
+
+    return res;
+  }
+
 }
