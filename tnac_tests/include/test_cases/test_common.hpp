13d77320ed729211a400c98fb98a00420d82b2d6
darckore <darckore@gmail.com>
2023-06-16 21:09:42 +0300

    tests on files

--- tnac_tests/include/test_cases/test_common.hpp
+++ tnac_tests/include/test_cases/test_common.hpp
@@ -1,6 +1,5 @@
 #pragma once
-#include "parser/parser.hpp"
-#include "sema/sema.hpp"
+#include "packages/core.hpp"
 
 namespace tnac_tests
 {
@@ -25,5 +24,101 @@ namespace tnac_tests
       tnac::sema sema;
       tnac::parser parser;
     };
+
+    using cplx = tnac::complex_type;
+    using frac = tnac::fraction_type;
+
+    template <tnac::eval::detail::expr_result T>
+    inline bool eq(const T& l, const T& r) noexcept
+    {
+      return l == r;
+    }
+
+    template <>
+    inline bool eq(const tnac::float_type& l, const tnac::float_type& r) noexcept
+    {
+      if (std::isinf(l) && std::isinf(r))
+        return true;
+
+      if (std::isnan(l) && std::isnan(r))
+        return true;
+
+      return utils::eq(l, r);
+    }
+
+    template <>
+    inline bool eq(const cplx& l, const cplx& r) noexcept
+    {
+      return eq(l.real(), r.real()) && eq(l.imag(), r.imag());
+    }
+
+    inline testing::Message& operator<<(testing::Message& msg, const frac& f) noexcept
+    {
+      if (f.sign() < 0) msg << '-';
+      msg << f.num() << ',' << f.denom();
+      return msg;
+    }
+
+    inline auto parse_input(string_t input, tnac::eval::registry& reg, tnac::eval::call_stack& cs) noexcept
+    {
+      parse_helper p;
+      p.parser(input);
+      tnac::evaluator ev{ reg, cs };
+      ev(p.parser.root());
+
+      return reg.evaluation_result();
+    }
+
+    inline constexpr auto invalid_value() noexcept
+    {
+      return tnac::eval::invalid_val_t{};
+    }
+
+    template <typename T>
+    concept testable_val = tnac::eval::detail::expr_result<T> ||
+                           tnac::is_same_noquals_v<T, tnac::eval::invalid_val_t>;
+
+    template <testable_val T>
+    inline void verify(tnac::eval::value val, T expected) noexcept
+    {
+      if constexpr (tnac::is_same_noquals_v<T, tnac::eval::invalid_val_t>)
+      {
+        ASSERT_TRUE(!val);
+      }
+      else
+      {
+        tnac::eval::on_value(val, [expected](auto val) noexcept
+          {
+            using expected_t = decltype(val);
+            if constexpr (tnac::is_same_noquals_v<expected_t, tnac::eval::invalid_val_t>)
+            {
+              ASSERT_TRUE(false) << "Undefined value detected";
+            }
+            else if constexpr (!tnac::is_same_noquals_v<expected_t, T>)
+            {
+              ASSERT_TRUE(false) << "Wrong value type";
+            }
+            else
+            {
+              ASSERT_TRUE(eq(expected, val)) << "expected: " << expected << " got: " << val;
+            }
+          });
+      }
+    }
+
+    template <tnac::eval::detail::expr_result T>
+    inline void check_eval(string_t input, T expected) noexcept
+    {
+      tnac::eval::registry reg;
+      tnac::eval::call_stack cs{ 10 };
+      verify(parse_input(input, reg, cs), expected);
+    }
+
+    inline void check_invalid(string_t input) noexcept
+    {
+      tnac::eval::registry reg;
+      tnac::eval::call_stack cs{ 0 };
+      verify(parse_input(input, reg, cs), invalid_value());
+    }
   }
 }
---
+++ tnac_tests/src/test_cases/test_program.cpp
@@ -0,0 +1,88 @@
+#include "test_cases/test_common.hpp"
+
+namespace tnac_tests
+{
+  namespace detail
+  {
+    namespace
+    {
+      using tnac::string_t;
+      using tnac::buf_t;
+
+      void read_file(string_t fname, buf_t& buf) noexcept
+      {
+        fsys::path fn{ fname };
+
+        std::error_code errc;
+        fn = fsys::absolute(fn, errc);
+        ASSERT_FALSE(static_cast<bool>(errc)) << "Bad file name " << fname;
+        std::ifstream in{ fn.string() };
+        ASSERT_TRUE(static_cast<bool>(in));
+        in.seekg(0, std::ios::end);
+        buf.reserve(in.tellg());
+        in.seekg(0, std::ios::beg);
+        using it = std::istreambuf_iterator<tnac::buf_t::value_type>;
+        buf.assign(it{ in }, it{});
+      }
+
+      void on_eval_error(const tnac::token& tok, string_t msg) noexcept
+      {
+        FAIL() << "Eval error " << msg << " at " << tok.m_value;
+      }
+
+      void on_parse_error(const tnac::ast::error_expr& err) noexcept
+      {
+        FAIL() << "Parse error " << err.message() << " at " << err.pos().m_value;
+      }
+
+      template <testable_val T>
+      void parse_file(string_t fname, T expected) noexcept
+      {
+        buf_t input;
+        read_file(fname, input);
+        ASSERT_FALSE(input.empty());
+
+        using tnac::packages::tnac_core;
+        tnac_core core{ 100 };
+
+        auto&& ev = core.get_eval();
+        auto&& parser = core.get_parser();
+        ev.on_error(on_eval_error);
+        parser.on_parse_error(on_parse_error);
+
+        parser(input);
+        ev(parser.root());
+        verify(ev.last_result(), expected);
+      }
+    }
+  }
+
+  #define TEST_EXAMPLE(N) "tests/example"#N".tnac"
+
+  namespace
+  {
+    using detail::frac;
+    using detail::cplx;
+    using detail::parse_file;
+  }
+
+  TEST(program, t_example0)
+  {
+    parse_file(TEST_EXAMPLE(0), cplx{ 0, 1 });
+  }
+
+  TEST(program, t_example1)
+  {
+    parse_file(TEST_EXAMPLE(1), frac{ 55 });
+  }
+
+  TEST(program, t_example2)
+  {
+    parse_file(TEST_EXAMPLE(2), 15ll);
+  }
+
+  TEST(program, t_example3)
+  {
+    parse_file(TEST_EXAMPLE(3), cplx{});
+  }
+}
