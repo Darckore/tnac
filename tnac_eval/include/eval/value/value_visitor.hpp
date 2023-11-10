6b740e6d3fec3164bdd4a369c3737d11b366eace
darckore <darckore@gmail.com>
2023-11-10 15:56:13 +0300

    moving trash around

--- tnac_eval/include/eval/value/value_visitor.hpp
+++ tnac_eval/include/eval/value/value_visitor.hpp
@@ -77,13 +77,10 @@ namespace tnac::eval
   public:
     CLASS_SPECIALS_NONE(value_visitor);
 
-    explicit value_visitor(registry& reg) noexcept :
-      m_registry{ reg }
-    {}
-
+    ~value_visitor() noexcept;
+    explicit value_visitor(registry& reg) noexcept;
 
   private: // Unary operations
-
     //
     // Registers result of unary operations
     //
@@ -184,7 +181,6 @@ namespace tnac::eval
     }
 
   private: // Binary operations
-
     //
     // Registers result of binary operations
     //
@@ -735,177 +731,77 @@ namespace tnac::eval
     //
     // Stores a value for a function
     //
-    value make_function(id_param_t ent, function_type f) noexcept
-    {
-      VALUE_GUARD(m_curEntity, *ent);
-      reg_value(f);
-      return m_registry.value_for(m_curEntity);
-    }
+    value make_function(id_param_t ent, function_type f) noexcept;
 
     //
     // Evaluates a binary expr
     //
-    void visit_binary(value lhs, value rhs, val_ops op) noexcept
-    {
-      if (!detail::is_binary(op))
-        clear_result();
-
-      visit_value(lhs, [this, rhs, op](auto lhs) noexcept
-        {
-          visit_binary(std::move(lhs), std::move(rhs), op);
-        });
-    }
+    void visit_binary(value lhs, value rhs, val_ops op) noexcept;
 
     //
     // Evaluates a unary expr
     //
-    void visit_unary(value val, val_ops op) noexcept
-    {
-      if (!detail::is_unary(op))
-      {
-        clear_result();
-        return;
-      }
-
-      visit_value(val, [this, op](auto v) noexcept
-        {
-          visit_unary(std::move(v), op);
-        });
-    }
+    void visit_unary(value val, val_ops op) noexcept;
 
     //
     // Pushes the given value to the stack
     //
-    void push_value(value val) noexcept
-    {
-      visit_value(val, [this](auto v) noexcept
-        {
-          reg_value(std::move(v));
-        });
-    }
+    void push_value(value val) noexcept;
 
     //
     // Pushes the last result to the stack
     //
-    void push_last() noexcept
-    {
-      push_value(last());
-    }
+    void push_last() noexcept;
 
     //
     // Makes a value for an assigned-to entity
     //
-    value visit_assign(id_param_t ent, value rhs) noexcept
-    {
-      VALUE_GUARD(m_curEntity, *ent);
-
-      visit_value(rhs, [this](auto v) noexcept
-        {
-          visit_assign(std::move(v));
-        });
-
-      return m_registry.value_for(m_curEntity);
-    }
+    value visit_assign(id_param_t ent, value rhs) noexcept;
 
     //
     // Parses an integer literal value from string
     //
-    void visit_int_literal(string_t src, int base) noexcept
-    {
-      auto prefix = string_t::size_type{};
-      if (utils::eq_any(base, 2, 16))
-        prefix = 2u;
-      else if (base == 8)
-        prefix = 1u;
-
-      auto begin = src.data() + prefix;
-      auto end = begin + src.length();
-
-      int_type result{};
-      auto convRes = std::from_chars(begin, end, result, base);
-      if (convRes.ec != std::errc{ 0 })
-      {
-        clear_result();
-        return;
-      }
-
-      reg_value(result);
-    }
+    void visit_int_literal(string_t src, int base) noexcept;
 
     //
     // Parses a floating point literal value from string
     //
-    void visit_float_literal(string_t src) noexcept
-    {
-      auto begin = src.data();
-      auto end = begin + src.length();
-
-      float_type result{};
-      auto convRes = std::from_chars(begin, end, result);
-      if (convRes.ec != std::errc{ 0 })
-      {
-        clear_result();
-        return;
-      }
-
-      reg_value(result);
-    }
+    void visit_float_literal(string_t src) noexcept;
 
     //
     // Registers a boolean literal
     //
-    void visit_bool_literal(bool value) noexcept
-    {
-      return reg_value(value);
-    }
+    void visit_bool_literal(bool value) noexcept;
 
     //
     // Registers an imaginary 'i' value
     //
-    void visit_i() noexcept
-    {
-      return reg_value(complex_type{ 0, 1 });
-    }
+    void visit_i() noexcept;
 
     //
     // Registers the value of pi
     //
-    void visit_pi() noexcept
-    {
-      return reg_value(std::numbers::pi_v<float_type>);
-    }
+    void visit_pi() noexcept;
 
     //
     // Registers the value of pi
     //
-    void visit_e() noexcept
-    {
-      return reg_value(std::numbers::e_v<float_type>);
-    }
+    void visit_e() noexcept;
 
     //
     // Retrieves the next value from the stack of temporaries
     //
-    stored_value fetch_next() noexcept
-    {
-      return m_registry.consume();
-    }
+    stored_value fetch_next() noexcept;
 
     //
     // Retrieves the last evaluation result
     //
-    value last() noexcept
-    {
-      return m_registry.evaluation_result();
-    }
+    value last() noexcept;
 
     //
     // Resets the last evaluation result and returns an empty value
     //
-    void clear_result() noexcept
-    {
-      reg_value(eval::invalid_val_t{});
-    }
+    void clear_result() noexcept;
 
     //
     // Fills the specified number of arg values in the given container
@@ -921,26 +817,14 @@ namespace tnac::eval
     //
     // Creates and registers an array
     //
-    void make_array(size_type count) noexcept
-    {
-      auto arr = m_registry.make_array(count);
-      arr->resize(count);
-      fill_args(*arr, count);
-      m_registry.push_array(arr);
-    }
+    void make_array(size_type count) noexcept;
 
     //
     // Creates a temporary local array and returns it without pushing it into
     // the registry. The result is pre-filled with args from the current stack
     // This is required for call expressions
     //
-    arr_t collect_args_locally(size_type count) noexcept
-    {
-      arr_t arr;
-      arr.resize(count);
-      fill_args(arr, count);
-      return arr;
-    }
+    arr_t collect_args_locally(size_type count) noexcept;
 
   private:
     static constexpr auto invalidEnt = detail::ent_id::invalid_id();
---
+++ tnac_eval/src/value_visitor.cpp
@@ -0,0 +1,167 @@
+//
+// Value visitor
+//
+
+#pragma once
+#include "eval/value/value_visitor.hpp"
+
+namespace tnac::eval
+{
+  // Special members
+
+  value_visitor::~value_visitor() noexcept = default;
+
+  value_visitor::value_visitor(registry& reg) noexcept :
+    m_registry{ reg }
+  {}
+
+
+  // Public members
+
+  value value_visitor::make_function(id_param_t ent, function_type f) noexcept
+  {
+    VALUE_GUARD(m_curEntity, *ent);
+    reg_value(f);
+    return m_registry.value_for(m_curEntity);
+  }
+
+  void value_visitor::visit_binary(value lhs, value rhs, val_ops op) noexcept
+  {
+    if (!detail::is_binary(op))
+      clear_result();
+
+    visit_value(lhs, [this, rhs, op](auto lhs) noexcept
+      {
+        visit_binary(std::move(lhs), std::move(rhs), op);
+      });
+  }
+
+  void value_visitor::visit_unary(value val, val_ops op) noexcept
+  {
+    if (!detail::is_unary(op))
+    {
+      clear_result();
+      return;
+    }
+
+    visit_value(val, [this, op](auto v) noexcept
+      {
+        visit_unary(std::move(v), op);
+      });
+  }
+
+  void value_visitor::push_value(value val) noexcept
+  {
+    visit_value(val, [this](auto v) noexcept
+      {
+        reg_value(std::move(v));
+      });
+  }
+
+  void value_visitor::push_last() noexcept
+  {
+    push_value(last());
+  }
+
+  value value_visitor::visit_assign(id_param_t ent, value rhs) noexcept
+  {
+    VALUE_GUARD(m_curEntity, *ent);
+
+    visit_value(rhs, [this](auto v) noexcept
+      {
+        visit_assign(std::move(v));
+      });
+
+    return m_registry.value_for(m_curEntity);
+  }
+
+  void value_visitor::visit_int_literal(string_t src, int base) noexcept
+  {
+    auto prefix = string_t::size_type{};
+    if (utils::eq_any(base, 2, 16))
+      prefix = 2u;
+    else if (base == 8)
+      prefix = 1u;
+
+    auto begin = src.data() + prefix;
+    auto end = begin + src.length();
+
+    int_type result{};
+    auto convRes = std::from_chars(begin, end, result, base);
+    if (convRes.ec != std::errc{ 0 })
+    {
+      clear_result();
+      return;
+    }
+
+    reg_value(result);
+  }
+
+  void value_visitor::visit_float_literal(string_t src) noexcept
+  {
+    auto begin = src.data();
+    auto end = begin + src.length();
+
+    float_type result{};
+    auto convRes = std::from_chars(begin, end, result);
+    if (convRes.ec != std::errc{ 0 })
+    {
+      clear_result();
+      return;
+    }
+
+    reg_value(result);
+  }
+
+  void value_visitor::visit_bool_literal(bool value) noexcept
+  {
+    return reg_value(value);
+  }
+
+  void value_visitor::visit_i() noexcept
+  {
+    return reg_value(complex_type{ 0, 1 });
+  }
+
+  void value_visitor::visit_pi() noexcept
+  {
+    return reg_value(std::numbers::pi_v<float_type>);
+  }
+
+  void value_visitor::visit_e() noexcept
+  {
+    return reg_value(std::numbers::e_v<float_type>);
+  }
+
+  stored_value value_visitor::fetch_next() noexcept
+  {
+    return m_registry.consume();
+  }
+
+  value value_visitor::last() noexcept
+  {
+    return m_registry.evaluation_result();
+  }
+
+  void value_visitor::clear_result() noexcept
+  {
+    reg_value(eval::invalid_val_t{});
+  }
+
+  void value_visitor::make_array(size_type count) noexcept
+  {
+    auto arr = m_registry.make_array(count);
+    arr->resize(count);
+    fill_args(*arr, count);
+    m_registry.push_array(arr);
+  }
+
+  value_visitor::arr_t value_visitor::collect_args_locally(size_type count) noexcept
+  {
+    arr_t arr;
+    arr.resize(count);
+    fill_args(arr, count);
+    return arr;
+  }
+
+}
