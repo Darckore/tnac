1c57060522540df3ac6854bfc0015d38624fd893
darckore <darckore@gmail.com>
2023-12-05 14:01:11 +0300

    removed driver include

--- tnac_runtime/include/driver/entry.hpp
+++ tnac_runtime/include/driver/entry.hpp
@@ -4,7 +4,6 @@
 //
 
 #pragma once
-#include "driver/driver.hpp"
 
 namespace tnac::rt
 {
--- tnac_runtime/src/driver/entry.cpp
+++ tnac_runtime/src/driver/entry.cpp
@@ -1,4 +1,5 @@
 #include "driver/entry.hpp"
+#include "driver/driver.hpp"
 
 #if TNAC_WINDOWS
   #define WIN32_LEAN_AND_MEAN
