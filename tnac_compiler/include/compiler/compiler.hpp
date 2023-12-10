ae418910c7672bc9505304f196cfb5abb7273386
darckore <darckore@gmail.com>
2023-12-10 15:58:51 +0300

    compiler is an ast visitor

--- tnac_compiler/include/compiler/compiler.hpp
+++ tnac_compiler/include/compiler/compiler.hpp
@@ -3,17 +3,13 @@
 //
 
 #pragma once
+#include "parser/ast/ast_visitor.hpp"
 #include "cfg/cfg.hpp"
 
 namespace tnac
 {
   class sema;
   class feedback;
-
-  namespace ast
-  {
-    class node;
-  }
 }
 
 namespace tnac
@@ -21,12 +17,14 @@ namespace tnac
   //
   // Walks the AST and produces a control flow graph
   //
-  class compiler final
+  class compiler final : public ast::bottom_up_visitor<compiler>
   {
   public:
     using tree     = ast::node;
     using tree_ptr = tree*;
     using tree_ref = tree&;
+
+    using base_t = ast::bottom_up_visitor<compiler>;
 
   public:
     CLASS_SPECIALS_NONE(compiler);
@@ -53,6 +51,38 @@ namespace tnac
     // Returns a reference to the CFG
     //
     ir::cfg& cfg() noexcept;
+
+  public: // General
+    void visit(ast::root& root) noexcept;
+    void visit(ast::module_def& mod) noexcept;
+    void visit(ast::import_dir& imp) noexcept;
+    void visit(ast::scope& scope) noexcept;
+    void visit(ast::error_expr& err) noexcept;
+
+  public: // Exprs
+    void visit(ast::result_expr& res) noexcept;
+    void visit(ast::ret_expr& ret) noexcept;
+    void visit(ast::lit_expr& lit) noexcept;
+    void visit(ast::id_expr& id) noexcept;
+    void visit(ast::unary_expr& unary) noexcept;
+    void visit(ast::binary_expr& binary) noexcept;
+    void visit(ast::assign_expr& assign) noexcept;
+    void visit(ast::decl_expr& decl) noexcept;
+    void visit(ast::array_expr& arr) noexcept;
+    void visit(ast::paren_expr& paren) noexcept;
+    void visit(ast::abs_expr& abs) noexcept;
+    void visit(ast::typed_expr& typed) noexcept;
+    void visit(ast::call_expr& call) noexcept;
+    void visit(ast::matcher& matcher) noexcept;
+    void visit(ast::pattern& pattern) noexcept;
+    void visit(ast::cond_short& cond) noexcept;
+    void visit(ast::cond_expr& cond) noexcept;
+    void visit(ast::dot_expr& dot) noexcept;
+
+  public: // Decls
+    void visit(ast::var_decl& var) noexcept;
+    void visit(ast::param_decl& param) noexcept;
+    void visit(ast::func_decl& func) noexcept;
 
   private:
     sema* m_sema{};
--- tnac_compiler/src/compiler.cpp
+++ tnac_compiler/src/compiler.cpp
@@ -20,7 +20,7 @@ namespace tnac
 
   void compiler::operator()(tree_ref node) noexcept
   {
-    utils::unused(node);
+    base_t::operator()(&node);
   }
 
   const ir::cfg& compiler::cfg() const noexcept
@@ -31,4 +31,142 @@ namespace tnac
   {
     return FROM_CONST(cfg);
   }
+
+
+  // General
+
+  void compiler::visit(ast::root& root) noexcept
+  {
+    utils::unused(root);
+  }
+
+  void compiler::visit(ast::module_def& mod) noexcept
+  {
+    utils::unused(mod);
+  }
+
+  void compiler::visit(ast::import_dir& imp) noexcept
+  {
+    utils::unused(imp);
+  }
+
+  void compiler::visit(ast::scope& scope) noexcept
+  {
+    utils::unused(scope);
+  }
+
+  void compiler::visit(ast::error_expr& err) noexcept
+  {
+    utils::unused(err);
+  }
+
+  // Exprs
+
+  void compiler::visit(ast::result_expr& res) noexcept
+  {
+    utils::unused(res);
+  }
+
+  void compiler::visit(ast::ret_expr& ret) noexcept
+  {
+    utils::unused(ret);
+  }
+
+  void compiler::visit(ast::lit_expr& lit) noexcept
+  {
+    utils::unused(lit);
+  }
+
+  void compiler::visit(ast::id_expr& id) noexcept
+  {
+    utils::unused(id);
+  }
+
+  void compiler::visit(ast::unary_expr& unary) noexcept
+  {
+    utils::unused(unary);
+  }
+
+  void compiler::visit(ast::binary_expr& binary) noexcept
+  {
+    utils::unused(binary);
+  }
+
+  void compiler::visit(ast::assign_expr& assign) noexcept
+  {
+    utils::unused(assign);
+  }
+
+  void compiler::visit(ast::decl_expr& decl) noexcept
+  {
+    utils::unused(decl);
+  }
+
+  void compiler::visit(ast::array_expr& arr) noexcept
+  {
+    utils::unused(arr);
+  }
+
+  void compiler::visit(ast::paren_expr& paren) noexcept
+  {
+    utils::unused(paren);
+  }
+
+  void compiler::visit(ast::abs_expr& abs) noexcept
+  {
+    utils::unused(abs);
+  }
+
+  void compiler::visit(ast::typed_expr& typed) noexcept
+  {
+    utils::unused(typed);
+  }
+
+  void compiler::visit(ast::call_expr& call) noexcept
+  {
+    utils::unused(call);
+  }
+
+  void compiler::visit(ast::matcher& matcher) noexcept
+  {
+    utils::unused(matcher);
+  }
+
+  void compiler::visit(ast::pattern& pattern) noexcept
+  {
+    utils::unused(pattern);
+  }
+
+  void compiler::visit(ast::cond_short& cond) noexcept
+  {
+    utils::unused(cond);
+  }
+
+  void compiler::visit(ast::cond_expr& cond) noexcept
+  {
+    utils::unused(cond);
+  }
+
+  void compiler::visit(ast::dot_expr& dot) noexcept
+  {
+    utils::unused(dot);
+  }
+
+  // Decls
+
+  void compiler::visit(ast::var_decl& var) noexcept
+  {
+    utils::unused(var);
+  }
+
+  void compiler::visit(ast::param_decl& param) noexcept
+  {
+    utils::unused(param);
+  }
+
+  void compiler::visit(ast::func_decl& func) noexcept
+  {
+    utils::unused(func);
+  }
+
 }
